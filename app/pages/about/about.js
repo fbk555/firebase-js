8e985238b3d51e5f93dcd727618e0ccf2f4003f9
derFaizio <aslam.mfaizan@gmail.com>
2017-03-21 17:22:14 +0100

    Google Authentication working Need to change the build.gradle file. See the documentation

--- app/pages/about/about.js
+++ app/pages/about/about.js
@@ -1,7 +1,22 @@
 var BasePage = require("../../shared/BasePage");
+var firebase = require("nativescript-plugin-firebase");
 
 var AboutPage = function() {};
 AboutPage.prototype = new BasePage();
 AboutPage.prototype.constructor = AboutPage;
+
+AboutPage.prototype.googleSignIn = function(){
+  console.log("Testing Google Sign In");
+  firebase.login({
+    type: firebase.LoginType.GOOGLE
+  }).then(
+      function (result) {
+        JSON.stringify(result);
+      },
+      function (errorMessage) {
+        console.log(errorMessage);
+      }
+  );
+};
 
 module.exports = new AboutPage();
